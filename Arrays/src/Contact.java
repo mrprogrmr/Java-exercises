6d080b48424b17039bebacc4ea8e3471d634a0b7
Guvanch Jumayev <gugalijumayew@gmail.com>
2024-12-07 01:16:17 +0100

    continue coding...

---
+++ Arrays/src/Contact.java
@@ -0,0 +1,26 @@
+public class Contact {
+
+	private String name;
+	private String phoneNumber;
+
+	public Contact(String name, String phoneNumber) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	public static createContact(String name, String phoneNumber) {
+
+	}
+
+	public String getName() {
+		return null;
+	}
+
+	public Object getPhoneNumber() {
+	}
+
+}
---
+++ Arrays/src/MultipleArrays.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class MultipleArrays {
+	public static void main(String[] args) {
+
+		int[][] array2 = new int[4][4];
+		System.out.println(Arrays.toString(array2));
+		System.out.println("array2.length = " + array2.length);
+
+//		for (int[] outer : array2) {
+//			System.out.println(Arrays.toString(outer));
+//		}
+
+		for (int i = 0; i < array2.length; i++) {
+			var innerArray = array2[i];
+			for (int j = 0; j < innerArray.length; j++) {
+				System.out.println(array2[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
